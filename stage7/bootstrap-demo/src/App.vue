0484322fa68702288f6747578f515d7a96782447
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-03-03 21:41:57 +0800

    update stage7 element UI

---
+++ stage7/bootstrap-demo/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+        <button class="btn btn-default">普通按钮</button>
+        <button class="btn btn-danger">红色按钮</button>
+        <div class="row">
+            <div class="col-sm-8 my-border">8</div>
+            <div class="col-sm-4 my-border">4</div>
+        </div>
+        <p>
+            sss
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data () {
+            return {}
+        }
+    }
+</script>
+
+<style scope lang="sass" rel="stylesheet/scss">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+
+        .my-border {
+            border: 1px solid red;
+        }
+
+    }
+
+
+</style>
