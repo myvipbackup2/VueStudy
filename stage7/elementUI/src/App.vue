fafab94ca751b9449a6814f2eceaabd76c21de56
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-03-03 22:34:08 +0800

    update stage7 element UI 尝试按钮

--- stage7/elementUI/src/App.vue
+++ stage7/elementUI/src/App.vue
@@ -1,45 +1,72 @@
 <template>
     <div id="app">
-        {{msg}}
+
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="text">文字按钮</el-button>
+        <div class="block">
+            <span class="demonstration">默认显示颜色</span>
+            <span class="wrapper">
+                <el-button type="success">成功按钮</el-button>
+                <el-button type="warning">警告按钮</el-button>
+                <el-button type="danger">危险按钮</el-button>
+                <el-button type="info">信息按钮</el-button>
+          </span>
+        </div>
+        <div class="block">
+            <span class="demonstration">hover 显示颜色</span>
+            <span class="wrapper">
+                <el-button :plain="true" type="success">成功按钮</el-button>
+                <el-button :plain="true" type="warning">警告按钮</el-button>
+                <el-button :plain="true" type="danger">危险按钮</el-button>
+                <el-button :plain="true" type="info">信息按钮</el-button>
+            </span>
+        </div>
+
+        <el-button type="primary" :loading="true">加载中</el-button>
+
     </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    export default {
+        name: 'app',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
     }
-  }
-}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" rel="stylesheet/scss">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+        .block {
+            margin: 20px;
+        }
+    }
 
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-a {
-  color: #42b983;
-}
+    a {
+        color: #42b983;
+    }
 </style>
